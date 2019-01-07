7fbd443a003590b1444e250c605b38ec6829e927
Sofyan Saputra <meongbego@gmail.com>
2019-01-07 09:52:36 +0700

    Add: Create Command

--- bless/clis/create.py
+++ bless/clis/create.py
@@ -1,15 +1,17 @@
 from bless.clis.base import Base
 from bless.libs import parsing as parse
-from bless.libs import utils
-import os
+from bless.libs import create_utils
+import os, json
 
 
 CURR_DIR = os.getcwd()
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 class Create(Base):
     """
         usage:
+            create
             create database
             create auth
             create config
@@ -28,16 +30,57 @@ class Create(Base):
     """
 
     def execute(self):
+        internet = create_utils.utils.check_internet()
+        url_bless_object = "https://raw.githubusercontent.com/Blesproject/bless_object/master/"
+       
         if self.args['database']:
-            check_yml = utils.read_file(CURR_DIR+"/database.yml")
+            check_yml = create_utils.utils.read_file(CURR_DIR+"/database.yml")
             if not check_yml:
-                pass
+                if not internet:
+                    value = ""
+                else:
+                    value = create_utils.utils.download(url_bless_object+"database.yml")
+                    value = create_utils.utils.yaml_parser(value.read())
+                file = create_utils.utils.yaml_create(value,CURR_DIR+"/database.yml")
             exit()
         if self.args['auth']:
-            print("auth")
+            check_yml = create_utils.utils.read_file(CURR_DIR+"/auth.yml")
+            if not check_yml:
+                if not internet:
+                    value = ""
+                else:
+                    value = create_utils.utils.download(url_bless_object+"auth.yml")
+                    value = create_utils.utils.yaml_parser(value.read())
+                file = create_utils.utils.yaml_create(value,CURR_DIR+"/auth.yml")
             exit()
         if self.args['config']:
-            print("Config")
+            check_yml = create_utils.utils.read_file(CURR_DIR+"/config.yml")
+            if not check_yml:
+                if not internet:
+                    value = ""
+                else:
+                    value = create_utils.utils.download(url_bless_object+"config.yml")
+                    value = create_utils.utils.yaml_parser(value.read())
+                file = create_utils.utils.yaml_create(value,CURR_DIR+"/config.yml")
             exit()
         if self.args['endpoint']:
-            print("Endpoint")
+            check_yml = create_utils.utils.read_file(CURR_DIR+"/endpoint.yml")
+            if not check_yml:
+                if not internet:
+                    value = ""
+                else:
+                    value = create_utils.utils.download(url_bless_object+"endpoint.yml")
+                    value = create_utils.utils.yaml_parser(value.read())
+                file = create_utils.utils.yaml_create(value,CURR_DIR+"/endpoint.yml")
+            exit()
+        
+        all_project = ['database','auth','config','endpoint']
+        for i in all_project:
+            value = None
+            if not internet:
+                value = ""
+            else:
+                value = create_utils.utils.download(url_bless_object+"/"+i+".yml")
+                value = create_utils.utils.yaml_parser(value.read())
+            file = create_utils.utils.yaml_create(value,CURR_DIR+"/"+i+".yml")
+        exit()
