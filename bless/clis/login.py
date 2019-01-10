4c60070dcca7bf8086564137ca274fe229cd69ab
Sofyan Saputra <meongbego@gmail.com>
2019-01-11 03:55:20 +0700

    Add: login method to deploy

---
+++ bless/clis/login.py
@@ -0,0 +1,20 @@
+from bless.clis.base import Base
+from bless.libs import login_utils
+import os
+
+
+CURR_DIR = os.getcwd()
+
+class Login(Base):
+    """
+        usage:
+            login
+
+        Build Project
+
+        Options:
+        -h --help                             Print usage
+    """
+
+    def execute(self):
+        print("LOGIN")
