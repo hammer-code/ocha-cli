33e0ae973096935b55cf1c0959ccd6ba8728841c
Sofyan Saputra <meongbego@gmail.com>
2019-01-16 20:38:43 +0700

    Edit: 80% To Fix Syncs Moduls Bussiness Logic

--- bless/libs/scp_utils.py
+++ bless/libs/scp_utils.py
@@ -6,12 +6,11 @@ CURR_DIR = os.getcwd()
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-deploy_data = utils.yaml_read(CURR_DIR+"/.deploy/deploy.ocha")
 
 
-def ssh_connect(host, username, port=None, password=None, key_filename=None):
+def ssh_connect(host, username, key_filename=None):
     try:
-        ssh.connect(host, username=username, port=port, password=password, key_filename=key_filename)
+        ssh.connect(host, username=username, key_filename=key_filename)
     except Exception as e:
         print(e)
         raise e
@@ -19,12 +18,9 @@ def ssh_connect(host, username, port=None, password=None, key_filename=None):
         return ssh
 
 
-def sync_file(path_file, path_remote):
-    host = deploy_data['ip']
-    username = deploy_data['username']
-    key = utils.read_value(CURR_DIR+"/.deploy/ssh_key.pem")
-    ssh = ssh_connect(host, username, key_filename=key)
-    ftp_client= ssh.open_sftp()
+def sync_file(ftp_client,path_file, path_remote):
     ftp_client.put(path_file, path_remote)
-    ftp_client.close()
-    ssh.close()
+
+def remote_server(ssh,command):
+    ssh.exec_command(command)
+    return ssh
