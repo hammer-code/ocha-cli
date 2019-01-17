59e4294316d177097ec0441cf018286c22d7dcbb
Sofyan Saputra <meongbego@gmail.com>
2019-01-17 21:14:42 +0700

    Add: neo activate and deactivate service

---
+++ bless/clis/neo.py
@@ -0,0 +1,21 @@
+from bless.clis.base import Base
+from bless.libs import parsing as parse
+import os
+
+class Neo(Base): 
+    """
+        usage:
+            neo activate
+            neo deactivate
+
+        Options:
+        -h --help                             Print usage
+    """
+
+    def execute(self):
+        if self.args['activate']:
+            print("ACTIVATED NEO SERVICE")
+            exit()
+        if self.args['deactivate']:
+            print("DEACTIVATED NEO SERVICE")
+            exit()
